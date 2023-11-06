c73a267ff4c6ef21af244a3bc01cb296a183a9d6
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-06 21:59:44 +0200

    Form submit server component

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,13 +1,11 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Text,
-  TextFieldInput,
-  TextFieldRoot,
-  TextFieldSlot,
-} from "@radix-ui/themes";
+import {Box,Button,Heading,Text,TextFieldInput,TextFieldRoot,} from "@radix-ui/themes";
 import Link from "next/link";
+
+async function createTodo(data: FormData) {
+  "use server"
+
+  console.log("Hello");
+}
 
 export default function NewTodo() {
   return (
@@ -23,12 +21,12 @@ export default function NewTodo() {
           <Text color="blue">Add new todo</Text>
         </Box>
       </Heading>
-      <Box className="flex justify-center pt-5 gap-x-4">
+      <form action={createTodo} className="flex justify-center pt-5 gap-x-4">
         <TextFieldRoot>
-          <TextFieldInput placeholder="Todo title" />
+          <TextFieldInput placeholder="Todo title"  />
         </TextFieldRoot>
         <Button color="blue">Add</Button>
-      </Box>
+      </form>
     </Box>
   );
 }
