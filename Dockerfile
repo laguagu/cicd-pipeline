6fd7b33b8ce31fbbbe41e00f0e02d05261396df8
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-09 20:30:05 +0200

    Containers run witouth prisma code

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,21 @@ FROM node:20
 # Määritä työskentelyhakemisto kuvaan
 WORKDIR /app
 
-# Kopioi projektin tiedostot (esim. package.json ja package-lock.json) työskentelyhakemistoon
+# Kopioi projektin riippuvuustiedostot (esim. package.json ja package-lock.json) työskentelyhakemistoon
 COPY package*.json ./
 
 # Asenna sovelluksen riippuvuudet
 RUN npm install
 
+# Kopioi Prisma-kansio ja schema.prisma tiedosto (tai koko projektin tiedostot, jos tarpeen)
+COPY prisma ./prisma
+
 # Kopioi muut projektin tiedostot työskentelyhakemistoon
 COPY . .
 
-RUN npx prisma db push
+# Generoi Prisma-client
+RUN npx prisma generate
+
 # Määritä kuinka sovellus käynnistetään (esim. Node.js-sovelluksissa)
 EXPOSE 3000  
 CMD ["npm", "run", "dev"]
--- app/new/page.tsx
+++ app/new/page.tsx
@@ -10,22 +10,22 @@ import {
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-async function createTodo(data: FormData) {
-  "use server";
+// async function createTodo(data: FormData) {
+//   "use server";
 
-  const title = data.get("title")
-  if (typeof title !== "string" || title.length <= 0) {
-    throw new Error("Invalid title!")
-  }
+//   const title = data.get("title")
+//   if (typeof title !== "string" || title.length <= 0) {
+//     throw new Error("Invalid title!")
+//   }
 
-  await prisma.todo.create({
-    data: {
-      title: title,
-      completed: false
-    }
-  })
-  redirect("/")
-}
+//   await prisma.todo.create({
+//     data: {
+//       title: title,
+//       completed: false
+//     }
+//   })
+//   redirect("/")
+// }
 
 export default function NewTodo() {
   return (
@@ -41,12 +41,12 @@ export default function NewTodo() {
           <Text color="blue">Add new todo</Text>
         </Box>
       </Heading>
-      <form action={createTodo} className="flex justify-center pt-5 gap-x-4">
+      {/* <form action={createTodo} className="flex justify-center pt-5 gap-x-4">
         <TextFieldRoot>
           <TextFieldInput placeholder="Todo title" name="title"/>
         </TextFieldRoot>
         <Button color="blue" type="submit">Add</Button>
-      </form>
+      </form> */}
     </Box>
   );
 }
